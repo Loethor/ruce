e67e7aaf2472c3cb9b945051b3f0c79e84c22c09
loethor <lrb2392@gmail.com>
2023-08-08 11:51:42 +0200

    Improved pawn tests

--- src/board/piece/pawn.rs
+++ src/board/piece/pawn.rs
@@ -238,12 +238,12 @@ mod tests {
         let row = 6;
         let col = 3;
         let square = row * BOARD_SIZE + col;
-        let piece = Piece {
+        let pawn = Piece {
             piece_type: Pawn,
             color: Black,
         };
-        board.set_piece(square, piece);
-        let moves = piece.generate_moves(&board, row, col).unwrap();
+        board.set_piece(square, pawn);
+        let moves = pawn.generate_moves(&board, row, col).unwrap();
 
         assert_eq!(moves.len(), 2);
 
